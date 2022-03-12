4a8198271886da4d24e2f8ef2d9a7423096351df
n-kev1n <kevin_@tuta.io>
2022-03-12 13:36:19 +0100

    project stylings

---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  primaryColor: "#2a9d8f",
+  secondaryColor: "#e9c46a",
+  primaryTextColor: "#aaa",
+  secondaryTextColor: "#7a8288",
+  tertiaryTextColor: "#272b30",
+  backgroundColor: "#272b30",
+  backgroundColor2: "#32383e",
+};
